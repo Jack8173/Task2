603afcca50f07bcb017278653217a8bfa120dbaf
Jack8173 <e.m.589@mail.ru>
2023-04-10 19:00:40 +0300

    Create Task2.cs

---
+++ Task2.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte playerHealth = 100;
+            sbyte modHealth = -1;
+            short modArmor = -100;
+            ushort Armor = 1000;
+            uint playerMoney = 50000;
+            float playerAttackRange = 9.1f;
+            char heart = '♥';
+            string playerName = "Grisha";
+            bool death = false;
+            ulong lifeTime;
+        }
+    }
+}
